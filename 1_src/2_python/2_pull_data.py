4613c9b158dc52a234b2e65b2e86f539fbd34a4c
JJ Espinoza <espin086@gmail.com>
2017-12-18 08:58:53 -0800

    update

--- 1_src/2_python/2_pull_data.py
+++ 1_src/2_python/2_pull_data.py
@@ -2,8 +2,8 @@
 
 
 import sys #used to facilitate arguments in terminal
-import requests
-from bs4 import BeautifulSoup
+import requests #used to download web pages
+from bs4 import BeautifulSoup #used to wrangle html text
 
 if sys.argv[1] == "--h":
 	print ''
@@ -19,8 +19,10 @@ if sys.argv[1] == "--h":
 	print '_______________________________________'
 	print ''
 	print '--h                           help menu'
-	print '--links ["url"]               scrapes hyperlinks on page'
 	print '--plots ["url"]               scrapes movie plots'
+	print '--links ["url"]               scrapes hyperlinks on page'
+	print '--wikisearch ["term"]         Finds wikis'
+	print '--wikiscrape ["term"]         Scrapes wikis'
 	print '_______________________________________'
 	print ''
 
@@ -36,14 +38,30 @@ if sys.argv[1] == "--links":
 		print(link.text, link.get('href'))
 
 #######################
+if sys.argv[1] == "--wikisearch":
+	print(wikipedia.search(sys.argv[2]))
+	page = wikipedia.page(sys.argv[2])
+	print(wikipedia.summary(sys.argv[2]))
+
+if sys.argv[1] == "--wikiscrape":
+	page = wikipedia.page(sys.argv[2])
+	print(page.title)
+	print(page.url)
+	print(page.content)
+
+
+#######################
 if sys.argv[1] == "--plots":
-	url = sys.argv[2]
-	#returns HTML content
-	r = requests.get(url)
-	#Converted into something useful
-	soup = BeautifulSoup(r.content)
-	#extract all hyperlinks for each link document
-	g_data = soup.find_all("div", {"id": "mw-content-txt"})
+
+	#################
+	#Using Beautiful Soup
+	# url = sys.argv[2]
+	# #returns HTML content
+	# r = requests.get(url)
+	# #Converted into something useful
+	# soup = BeautifulSoup(r.content)
+	# #extract all hyperlinks for each link document
+	# g_data = soup.find_all("div", {"id": "mw-content-txt"})
 
 	for item in g_data:
 		print(item.text)
