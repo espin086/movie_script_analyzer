bb8fa0641dcf94a04174837750123b4cf605d1b2
JJ Espinoza <jj.espinoza@fox.com>
2017-12-18 06:15:23 -0800

    anychanges

--- 1_src/1_shell/2_datapipes.sh
+++ 1_src/1_shell/2_datapipes.sh
@@ -13,6 +13,7 @@ then
     echo "./2_pull_data.sh --releases	Pulls data for future film releases"
     echo "./2_pull_data.sh --actors   Pulls data on actors films (num/box office)"
     echo "./2_pull_data.sh --box_current_month   Pulls box offie data to date"
+        echo "./2_pull_data.sh --pushtogoogle   Pushes data to Google cloud storage"
     echo ""
     echo "Current Directory is: "
     pwd
@@ -35,3 +36,5 @@ if [ $1 = "--box_current_month" ]
 then
 Rscript /Users/jje/Documents/00__mytools/3_mojo/1_src/2_r/3_monthly_box.R
 fi
+
+
